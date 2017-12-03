2073a39de2c41e8c281ff6ece0b1cc4db93d4624
Gildevan <33264837+Gildevan@users.noreply.github.com>
2017-12-03 17:25:01 -0200

    Elementos de hud

--- Colore John e Gil/Assets/Scripts/ColoreGM.cs
+++ Colore John e Gil/Assets/Scripts/ColoreGM.cs
@@ -10,30 +10,15 @@ public class ColoreGM : MonoBehaviour {
 	public static ColoreGM instance;
 	public GameObject restartButton;
 	public GameObject gameOverText;
-	public GameObject quitButton;
 	public bool gameOver = false;
 	public int Score = 0;
 
-	void Awake(){
-		if (instance == null) {
-			instance = this;
-
-		}
-
-		else if (instance != null) 
-		{
-
-			Destroy (gameObject);
-		}
-			
-	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		if (gameOver == true) {
 			restartButton.SetActive (true);
-			quitButton.SetActive (true);
 		}
 
 	}
