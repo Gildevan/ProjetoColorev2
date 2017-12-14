5b7fa21f6b14d15c647579789840fc3c92239e25
WinstonJohnNetinim <32879234+WinstonJohnNetinim@users.noreply.github.com>
2017-12-14 12:45:02 -0300

    script certo
    fahsdiufhsiufhiusdhfiush

--- Colore John e Gil/Assets/Scripts/MudancaDeFase.cs
+++ Colore John e Gil/Assets/Scripts/MudancaDeFase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TeleportToriiInicio : MonoBehaviour {
+public class MudancaDeFase : MonoBehaviour {
 
 
 	// Use this for initialization
